2304ce96c53ad39235b4f1b9d3daa14a12f4763b
enochmaonga <enoch.mokua@student.moringaschool.com>
2022-03-02 12:32:18 +0300

    adding images

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../App.css';
+import Header from './Header';
 
 function App() {
   return (
-     <React.Fragment>
-       <h1>Nyolis Products</h1>
+    <React.Fragment>
+    <div>
+       <Header/>
+     </div>
      </React.Fragment>
     
-  );
+  )
 }
 
 export default App;
