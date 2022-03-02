c0e81e5534578c18fe09004fa510eb9cc2c160eb
enochmaonga <enoch.mokua@student.moringaschool.com>
2022-03-02 09:15:34 +0300

    adding component directory, moving app.js to the component and creation of Header component

---
+++ src/compnents/Header.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+function Header() {
+   return (
+       <React.Fragment>
+           <div>Our Clothing, Your Comfort</div>
+           <div >
+               We understand that you need to look good and feel great. <br/>
+               It all starts with what you are wearing.                                
+           </div>
+       </React.Fragment>
+   )
+}
+
+export default Header;
