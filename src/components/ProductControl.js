9b2891053661e9594ea60b0b6eec585ebca104a6
enochmaonga <enoch.mokua@student.moringaschool.com>
2022-03-03 15:41:09 +0300

    Adding buttons for Add stock

--- src/components/ProductControl.js
+++ src/components/ProductControl.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import AddProduct from './AddProduct';
 import ProductList from './ProductList';
+import NewProductForm from './NewProductForm';
 
 class ProductControl extends Component {
    constructor(props){
        super(props);
-       this.state ={}
+       this.state ={
+           productFormVisible: false
+       }
+   }
+   handleClick = ()=>{
+       this.setState((prevState)=>({
+           productFormVisible: !prevState.productFormVisible
+       }))
    }
    render() {
+       let currentVisibleState = null;
+       let buttonText = null;
+    //    let addProductButton = null;
+       if (this.state.productFormVisible){
+           currentVisibleState = <NewProductForm />
+           buttonText = 'Go back to Product List'
+       } else{
+           currentVisibleState = <ProductList/>
+           buttonText = 'Add A Product';
+        //    addProductButton = <button onClick={this.handleClick}>Add Product</button>
+       }
        return (
            <React.Fragment>
-
+               <button onClick = {this.handleClick}>{buttonText} </button>
+               {currentVisibleState}
            </React.Fragment>
        )
    }
